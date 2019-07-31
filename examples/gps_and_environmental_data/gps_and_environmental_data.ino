1c0ccaee355fc24c7f2046a51f696fa1b8665038
luqmaanb <38786673+luqmaanb@users.noreply.github.com>
2019-07-31 14:19:32 +0200

    Update gps_and_environmental_data.ino

--- examples/gps_and_environmental_data/gps_and_environmental_data.ino
+++ examples/gps_and_environmental_data/gps_and_environmental_data.ino
@@ -1,193 +1,116 @@
-/*******************************************************************************
- *
- * This example sends a valid LoRaWAN packet with payload "Hello,
- * world!", using frequency and encryption settings matching those of
- * the The Things Network.
- *
- * This uses OTAA (Over-the-air activation), where where a DevEUI and
- * application key is configured, which are used in an over-the-air
- * activation procedure where a DevAddr and session keys are
- * assigned/generated for use with all further communication.
- *
- * Note: LoRaWAN per sub-band duty-cycle limitation is enforced (1% in
- * g1, 0.1% in g2), but not the TTN fair usage policy (which is probably
- * violated by this sketch when left running for longer)!
- 
- * To use this sketch, first register your application and device with
- * the things network, to set or generate an AppEUI, DevEUI and AppKey.
- * Multiple devices can use the same AppEUI, but each device has its own
- * DevEUI and AppKey.
- *
- * Do not forget to define the radio type correctly in config.h.
- *
- *******************************************************************************/
- 
-#include <lmic.h>
-#include <hal/hal.h>
-#include <SPI.h>
-#include <xSW02.h>
+// libary includes
+#include <xCore.h>
 #include <xSN01.h>
- 
-xSW02 SW02;
+#include <xSW01.h>
+#include <xPB04.h>
+
+// xchip instances
+xPB04 PB04;
 xSN01 SN01;
- 
-// This EUI must be in little-endian format, so least-significant-byte
-// first. When copying an EUI from ttnctl output, this means to reverse
-// the bytes. For TTN issued EUIs the last bytes should be 0xD5, 0xB3,
-// 0x70.
-static const u1_t PROGMEM APPEUI[8]={ 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-void os_getArtEui (u1_t* buf) { memcpy_P(buf, APPEUI, 8);}
- 
-// This should also be in little endian format, see above.
-static const u1_t PROGMEM DEVEUI[8]={ 0x01, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00 };
-void os_getDevEui (u1_t* buf) { memcpy_P(buf, DEVEUI, 8);}
- 
-// This key should be in big endian format (or, since it is not really a
-// number but a block of memory, endianness does not really apply). In
-// practice, a key taken from ttnctl can be copied as-is.
-// The key shown here is the semtech default key.
-static const u1_t PROGMEM APPKEY[16] = { 0x2B, 0x7E, 0x15, 0x16, 0x28, 0xAE, 0xD2, 0xA6, 0xAB, 0xF7, 0x15, 0x88, 0x09, 0xCF, 0x4F, 0x3C };
-void os_getDevKey (u1_t* buf) {  memcpy_P(buf, APPKEY, 16);}
- 
-static uint8_t mydata[] = "Hello, world!";
-static osjob_t sendjob;
- 
-// Schedule TX every this many seconds (might become longer due to duty
-// cycle limitations).
-const unsigned TX_INTERVAL = 60;
- 
-// Pin mapping
-const lmic_pinmap lmic_pins = {
-  .nss = 10,
-  .rxtx = LMIC_UNUSED_PIN,
-  .rst = LMIC_UNUSED_PIN,
-  .dio = {LMIC_UNUSED_PIN, LMIC_UNUSED_PIN, LMIC_UNUSED_PIN}, //2,4,na
-};
- 
-void onEvent (ev_t ev) {
-    Serial.print(os_getTime());
-    Serial.print(": ");
-    switch(ev) {
-        case EV_SCAN_TIMEOUT:
-            Serial.println(F("EV_SCAN_TIMEOUT"));
-            break;
-        case EV_BEACON_FOUND:
-            Serial.println(F("EV_BEACON_FOUND"));
-            break;
-        case EV_BEACON_MISSED:
-            Serial.println(F("EV_BEACON_MISSED"));
-            break;
-        case EV_BEACON_TRACKED:
-            Serial.println(F("EV_BEACON_TRACKED"));
-            break;
-        case EV_JOINING:
-            Serial.println(F("EV_JOINING"));
-            break;
-        case EV_JOINED:
-            Serial.println(F("EV_JOINED"));
- 
-            // Disable link check validation (automatically enabled
-            // during join, but not supported by TTN at this time).
-            LMIC_setLinkCheckMode(0);
-            break;
-        case EV_RFU1:
-            Serial.println(F("EV_RFU1"));
-            break;
-        case EV_JOIN_FAILED:
-            Serial.println(F("EV_JOIN_FAILED"));
-            break;
-        case EV_REJOIN_FAILED:
-            Serial.println(F("EV_REJOIN_FAILED"));
-            break;
-            break;
-        case EV_TXCOMPLETE:
-            Serial.println(F("EV_TXCOMPLETE (includes waiting for RX windows)"));
-            if (LMIC.txrxFlags & TXRX_ACK)
-              Serial.println(F("Received ack"));
-            if (LMIC.dataLen) {
-              Serial.println(F("Received "));
-              Serial.println(LMIC.dataLen);
-              Serial.println(F(" bytes of payload"));
-             Serial.println();
-            }
-            // Schedule next transmission
-            os_setTimedCallback(&sendjob, os_getTime()+sec2osticks(TX_INTERVAL), do_send);
-            break;
-        case EV_LOST_TSYNC:
-            Serial.println(F("EV_LOST_TSYNC"));
-            break;
-        case EV_RESET:
-            Serial.println(F("EV_RESET"));
-            break;
-        case EV_RXCOMPLETE:
-            // data received in ping slot
-            Serial.println(F("EV_RXCOMPLETE"));
-            break;
-        case EV_LINK_DEAD:
-            Serial.println(F("EV_LINK_DEAD"));
-            break;
-        case EV_LINK_ALIVE:
-            Serial.println(F("EV_LINK_ALIVE"));
-            break;
-         default:
-            Serial.println(F("Unknown event"));
-            break;
-    }
+xSW01 SW01;
+
+// TTN KEYS
+char *DEV_ADDR = "000081064825742E";
+char *APPSKEY = "70B3D57ED001A218";
+char *NWKSKEY = "3B9093E93C546818065011CDD0478F19";
+
+char debug = '1';
+
+#define CR02_I2C_ADDRESS 0x08 // slave address of CR02
+
+#define BUFLEN 18 // buffer size to send over i2c to CC03
+char data[BUFLEN];
+
+int32_t latitude;
+int32_t longitude;
+int16_t temperature;
+uint16_t pressure;
+uint16_t humidity;
+uint16_t voltage;
+uint16_t current;
+float hdop;
+void setup() {
+  delay(4000);
+  Wire.begin(); // begin i2c
+  SW01.begin(); // configure and start SW01
+  SN01.begin(); // configure and start SN02
+  PB04.begin(); // configure and start PB04
+  delay(2000);
+
+  // send ttn keys over to CR02
+  send_keys(APPSKEY, DEV_ADDR, NWKSKEY, debug);
+  delay(50);
 }
- 
-void do_send(osjob_t* j) {
-  SW02.poll();
+
+void loop() {
+  // poll sensors
+  PB04.poll();
+  SW01.poll();
   SN01.poll();
-  uint16_t temperature = SW02.getTempC() * 100;
-  uint16_t humidity = SW02.getHumidity() * 100;
-  uint32_t pressure = SW02.getPressure() * 100;
-  int32_t latitude = SN01.getLatitude() * 10000000;
-  int32_t longitude = SN01.getLatitude() * 10000000;
- 
-#define DATASIZE 16
-  static uint8_t data[DATASIZE];
+
+  // read sensor values
+  temperature = SW01.getTempC() * 100;
+  humidity = SW01.getHumidity() * 100;
+  pressure = SW01.getPressure() / 100;
+  voltage = PB04.getVoltage() * 100;
+  current = PB04.getCurrent();
+  hdop = SN01.getHDOP();
+
   data[0] = highByte(temperature);
   data[1] = lowByte(temperature);
   data[2] = highByte(humidity);
   data[3] = lowByte(humidity);
-  data[4] = (pressure >> 24) & 0xFF;
-  data[5] = (pressure >> 16) & 0xFF;
-  data[6] = (pressure >> 8) & 0xFF;
-  data[7] = pressure & 0xFF;
-  data[8] = (latitude >> 24) & 0xFF;
-  data[9] = (latitude >> 16) & 0xFF;
-  data[10] = (latitude >> 8) & 0xFF;
-  data[11] = latitude & 0xFF;
-  data[12] = (longitude >> 24) & 0xFF;
-  data[13] = (longitude >> 16) & 0xFF;
-  data[14] = (longitude >> 8) & 0xFF;
-  data[15] = longitude & 0xFF;
+  data[4] = highByte(pressure);
+  data[5] = lowByte(pressure);
 
-  // Check if there is not a current TX/RX job running
-  if (LMIC.opmode & OP_TXRXPEND) {
-    Serial.println(F("OP_TXRXPEND, not sending"));
+  if (hdop > 6.0) { // check hdop to ensure relaibale data, else provide default coordinates
+    latitude = SN01.getLatitude() * 10000000;
+    longitude = SN01.getLongitude() * 10000000;
   } else {
-    // Prepare transmission at the next possible time.
-    LMIC_setTxData2(1, data, DATASIZE, 0);
+    latitude = -340851700;
+    longitude = 188219427;
+  }
+  data[6] = (latitude >> 24) & 0xFF;
+  data[7] = (latitude >> 16) & 0xFF;
+  data[8] = (latitude >> 8) & 0xFF;
+  data[9] = latitude & 0xFF;
+  data[10] = (longitude >> 24) & 0xFF;
+  data[11] = (longitude >> 16) & 0xFF;
+  data[12] = (longitude >> 8) & 0xFF;
+  data[13] = longitude & 0xFF;
+  data[14] = highByte(voltage);
+  data[15] = lowByte(voltage);
+  data[16] = highByte(current);
+  data[17] = lowByte(current);
+
+  // send the data over i2c
+  send_data(data);
+  delay(4000);
+}
+
+void send_keys(char *APPSKEY, char *DEV_ADDR, char *NWKSKEY, char debug)
+{
+  char* keys[3] = {APPSKEY, DEV_ADDR, NWKSKEY};
+  Wire.beginTransmission(CR02_I2C_ADDRESS);
+  Wire.write(debug);
+  Wire.endTransmission();
+  delay(100);
+  for (int i = 0; i < 3; i++) {
+    Wire.beginTransmission(CR02_I2C_ADDRESS);
+    Wire.write('a' + i);
+    Wire.endTransmission();
+    delay(100);
+    Wire.beginTransmission(CR02_I2C_ADDRESS);
+    Wire.write(keys[i]);
+    Wire.endTransmission();
+    delay(100);
   }
 }
- 
-void setup() {
-    Serial.begin(115200);
-    Serial.println(F("Starting"));
-    Wire.begin();
-    SW02.begin();
-    SN01.begin();
-  
-    // LMIC init
-    os_init();
-    // Reset the MAC state. Session and pending data transfers will be discarded.
-    LMIC_reset();
- 
-    // Start job (sending automatically starts OTAA too)
-    do_send(&sendjob);
-}
- 
-void loop() {
-    os_runloop_once();
+
+void send_data(char*buf)
+{
+  Wire.beginTransmission(CR02_I2C_ADDRESS);
+  Wire.write('0');
+  Wire.write(buf, BUFLEN);
+  Wire.endTransmission();
 }
